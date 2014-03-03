6a9efc3845241e2958596cc694c2a4c4921dbb1b
ne_Sachirou <utakata.c4se@gmail.com>
2014-03-03 11:29:28 +0900

    Create store DTO and repository. Related #22, #23 and #24

---
+++ src/tk/c4se/halt/ih31/nimunimu/dto/Store.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package tk.c4se.halt.ih31.nimunimu.dto;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+/**
+ * @author ne_Sachirou
+ * 
+ */
+@Data
+public class Store implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String place;
+	private int goodsId;
+	private Goods goods;
+	private int goodsNumber;
+}
---
+++ src/tk/c4se/halt/ih31/nimunimu/repository/StoreRepository.java
@@ -0,0 +1,192 @@
+/**
+ * 
+ */
+package tk.c4se.halt.ih31.nimunimu.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Cleanup;
+import lombok.val;
+import tk.c4se.halt.ih31.nimunimu.config.DBConnector;
+import tk.c4se.halt.ih31.nimunimu.dto.Store;
+import tk.c4se.halt.ih31.nimunimu.exception.DBAccessException;
+
+/**
+ * @author ne_Sachirou
+ * 
+ */
+public class StoreRepository extends RdbRepository<Store> {
+	private static final long serialVersionUID = 1L;
+
+	public StoreRepository() {
+		super();
+	}
+
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * @throws DBAccessException
+	 */
+	public Store find(String place) throws DBAccessException {
+		val sql = "select * from store where place = ?";
+		Store store = null;
+		try (val connection = DBConnector.getConnection()) {
+			@Cleanup
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, place);
+			@Cleanup
+			val result = statement.executeQuery();
+			if (result.next()) {
+				store = new Store();
+				setProperties(store, result);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DBAccessException(e);
+		}
+		return store;
+	}
+
+	/**
+	 * 
+	 * @param page
+	 * @return
+	 * @throws DBAccessException
+	 */
+	public List<Store> all(int page) throws DBAccessException {
+		val sql = "select * from store where rownum between ? and ?";
+		List<Store> stores = new ArrayList<>();
+		try (val connection = DBConnector.getConnection()) {
+			@Cleanup
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setInt(1, perPage * (page - 1) + 1);
+			statement.setInt(2, perPage * page);
+			@Cleanup
+			val result = statement.executeQuery();
+			while (result.next()) {
+				Store store = new Store();
+				setProperties(store, result);
+				stores.add(store);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DBAccessException(e);
+		}
+		return stores;
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws DBAccessException
+	 */
+	public List<Store> all() throws DBAccessException {
+		return all(1);
+	}
+
+	public void insert(Store store) throws DBAccessException {
+		val sql = "insert into store(place, goods_id, goods_number) values (?, ?, ?)";
+		Connection connection = null;
+		try {
+			connection = DBConnector.getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, store.getPlace());
+			statement.setInt(2, store.getGoodsId());
+			statement.setInt(3, store.getGoodsNumber());
+			statement.executeUpdate();
+			connection.commit();
+		} catch (SQLException e) {
+			if (connection != null) {
+				try {
+					connection.rollback();
+				} catch (SQLException e1) {
+					throw new DBAccessException(e1);
+				}
+			}
+			throw new DBAccessException(e);
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					throw new DBAccessException(e);
+				}
+			}
+		}
+	}
+
+	public void update(Store store) throws DBAccessException {
+		val sql = "update store set goods_id = ?, goods_number = ? where place = ?";
+		Connection connection = null;
+		try {
+			connection = DBConnector.getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setInt(1, store.getGoodsId());
+			statement.setInt(2, store.getGoodsNumber());
+			statement.setString(3, store.getPlace());
+			statement.executeUpdate();
+			connection.commit();
+		} catch (SQLException e) {
+			if (connection != null) {
+				try {
+					connection.rollback();
+				} catch (SQLException e1) {
+					throw new DBAccessException(e1);
+				}
+			}
+			throw new DBAccessException(e);
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					throw new DBAccessException(e);
+				}
+			}
+		}
+	}
+
+	public void delete(Store store) throws DBAccessException {
+		val sql = "delete from store where place = ?";
+		Connection connection = null;
+		try {
+			connection = DBConnector.getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1, store.getPlace());
+			statement.executeUpdate();
+			connection.commit();
+		} catch (SQLException e) {
+			if (connection != null) {
+				try {
+					connection.rollback();
+				} catch (SQLException e1) {
+					throw new DBAccessException(e1);
+				}
+			}
+			throw new DBAccessException(e);
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					throw new DBAccessException(e);
+				}
+			}
+		}
+	}
+
+	@Override
+	protected Store setProperties(Store store, ResultSet result)
+			throws SQLException {
+		store.setPlace(result.getString("place"));
+		store.setGoodsId(result.getInt("goods_id"));
+		store.setGoodsNumber(result.getInt("goods_number"));
+		return store;
+	}
+}
