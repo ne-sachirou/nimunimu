ef14bf02e0afe6c1be0debf89998209d1a93ab3c
ne_Sachirou <utakata.c4se@gmail.com>
2014-03-04 16:03:24 +0900

    Modify SQLLoder ctl. This is the last ver with Ryo Morita.

---
+++ sql/testdata/sqlldr.rb
@@ -0,0 +1,23 @@
+%w{member
+notification
+customer
+supplier
+goods_category
+goods
+special_price_goods
+store
+stock_taking_result
+quotation_request_sheet
+quotation_request_sheet_detail
+customer_order_sheet
+customer_order_sheet_detail
+customer_order
+billing
+billing_detail
+our_order_sheet
+our_order_sheet_detail
+our_order
+payment
+payment_detail}.each do |name|
+  system "sqlldr.bat #{name}"
+end
